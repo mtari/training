aadf428aabb0468adf787850fc630167b95c4d48
Mihaly Tari <hodi@mihalys-mini.mynet>
2017-10-09 20:03:33 +0100

    Modify backbone app

--- Javascript/javascript-design-patterns/todomvc-demo/js/views/todo-view.js
+++ Javascript/javascript-design-patterns/todomvc-demo/js/views/todo-view.js
@@ -20,6 +20,7 @@ var app = app || {};
 			'click .toggle': 'toggleCompleted',
 			'dblclick label': 'edit',
 			'click .destroy': 'clear',
+			'click .edit-btn': 'edit',
 			'keypress .edit': 'updateOnEnter',
 			'keydown .edit': 'revertOnEscape',
 			'blur .edit': 'close'
@@ -49,6 +50,7 @@ var app = app || {};
 			}
 
 			this.$el.html(this.template(this.model.toJSON()));
+			// this.$el.value = "TEST";
 			this.$el.toggleClass('completed', this.model.get('completed'));
 			this.toggleVisible();
 			this.$input = this.$('.edit');
