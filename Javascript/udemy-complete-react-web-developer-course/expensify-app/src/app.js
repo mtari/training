687fd2f7035dc7f45f0c1d449d60314d46c94c21
Mihaly Tari <hodi@mihalys-airport-express.home>
2017-12-29 22:46:32 +0000

    Refactoring Routers

--- Javascript/udemy-complete-react-web-developer-course/expensify-app/src/app.js
+++ Javascript/udemy-complete-react-web-developer-course/expensify-app/src/app.js
@@ -1,31 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import AppRouter from './routers/AppRouter';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-const ExpenseDashboardPage = () => (
-    <div>This is dashboard</div>
-);
-
-const AddExpensePage = () => (
-    <div>This is create page</div>
-);
-
-const NotFoundPage = () => (
-    <div>Page not found</div>
-);
-
-const routes = (
-    <BrowserRouter>
-        <Switch>
-            <Route path='/' component={ExpenseDashboardPage} exact={true} />
-            <Route path='/create' component={AddExpensePage} />
-            <Route path='/edit' component={AddExpensePage} />
-            <Route path='/help' component={AddExpensePage} />
-            <Route component={NotFoundPage} />
-        </Switch>
-    </BrowserRouter>
-);
-
-ReactDOM.render(routes, document.getElementById('app'));
+ReactDOM.render(<AppRouter />, document.getElementById('app'));
--- Javascript/udemy-complete-react-web-developer-course/expensify-app/webpack.config.js
+++ Javascript/udemy-complete-react-web-developer-course/expensify-app/webpack.config.js
@@ -1,29 +1,28 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, "public"),
-        filename: 'bundle.js'
-    },
-    module: {
-        //To configure babel, so our JS is compiled to js, what the browser can run
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        }, {
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, "public"),
-        historyApiFallback: true
-    }
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      loader: 'babel-loader',
+      test: /\.js$/,
+      exclude: /node_modules/
+    }, {
+      test: /\.s?css$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        'sass-loader'
+      ]
+    }]
+  },
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    historyApiFallback: true
+  }
 };
