849f714190e03b7211509109ac1c978397764957
Mihaly Tari <hodi@mihalys-mini.home>
2018-02-25 19:56:52 +0000

    Added billing and authentication

--- Javascript/udemy-node-with-react-fullstack-web-development/server/index.js
+++ Javascript/udemy-node-with-react-fullstack-web-development/server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
+const bodyParser = require("body-parser");
 const keys = require("./config/keys");
 require("./models/User");
 require("./services/passport");
@@ -10,16 +11,29 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(
-    cookieSession({
-        maxAge: 30 * 24 * 60 * 60 * 1000,
-        keys: [keys.cookieKey]
-    })
+	cookieSession({
+		maxAge: 30 * 24 * 60 * 60 * 1000,
+		keys: [keys.cookieKey]
+	})
 );
 app.use(passport.initialize());
 app.use(passport.session());
 
 require("./routes/authRoutes")(app);
+require("./routes/billingRoutes")(app);
+
+if (process.env.NODE_ENV === "production") {
+	// Express will serve up production assets like our main.js file or main.css file!
+	app.use(express.static('client/build'));
+
+	// Express will serve up the index.html if it doesn't recognise the route
+	const path = require('path');
+	app.get('*', (req, res) => {
+		res.sendfile(path.resolve(__dirname, "client", "build", "index.html"));
+	})
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
